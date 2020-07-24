d3ef02ebe46b973d5a3c514b777b8686f25537c2
dharmesh95 <95dharmesh@gmail.com>
2020-07-24 11:09:18 -0400

    added service for tv shows

--- src/main/java/com/humber/soapwebservice/services/MoviesService.java
+++ src/main/java/com/humber/soapwebservice/services/MoviesService.java
@@ -8,6 +8,7 @@ package com.humber.soapwebservice.services;
 import com.humber.soapwebservice.controllers.MoviesJpaController;
 import com.humber.soapwebservice.models.Movies;
 import java.math.BigDecimal;
+import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -24,7 +25,7 @@ import javax.xml.ws.soap.MTOM;
 public class MoviesService {
 
     @WebMethod
-    public int addMovie(@WebParam(name = "moviename") String movieName, @WebParam(name = "image") byte[] image) throws Exception {
+    public Movies addMovie(@WebParam(name = "moviename") String movieName, @WebParam(name = "image") byte[] image) throws Exception {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
 
         MoviesJpaController movieRepo = new MoviesJpaController(emf);
@@ -36,6 +37,46 @@ public class MoviesService {
 
         movieRepo.create(movie);
 
-        return movieRepo.getMoviesCount() + 1;
+        return movie;
     }
+
+    @WebMethod
+    public List<Movies> getMovies() throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
+
+        MoviesJpaController movieRepo = new MoviesJpaController(emf);
+
+        List<Movies> findMoviesEntities = movieRepo.findMoviesEntities(10, 0);
+
+        return findMoviesEntities;
+    }
+
+    @WebMethod
+    public Movies updateMovie(@WebParam(name = "id") int id, @WebParam(name = "moviename") String movieName, @WebParam(name = "image") byte[] image) throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
+
+        MoviesJpaController movieRepo = new MoviesJpaController(emf);
+
+        Movies findMovies = movieRepo.findMovies(BigDecimal.valueOf(id));
+        movieRepo.destroy(BigDecimal.valueOf(id));
+
+        findMovies.setName(movieName);
+        findMovies.setImage(image);
+        findMovies.setId(BigDecimal.valueOf(movieRepo.getMoviesCount() + 1));
+
+        movieRepo.create(findMovies);
+
+        return findMovies;
+    }
+
+    @WebMethod
+    public int deleteMovieById(@WebParam(name = "id") int id) throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
+
+        MoviesJpaController movieRepo = new MoviesJpaController(emf);
+        movieRepo.destroy(BigDecimal.valueOf(id));
+
+        return id;
+    }
+
 }
--- src/main/java/com/humber/soapwebservice/services/TvShowsService.java
+++ src/main/java/com/humber/soapwebservice/services/TvShowsService.java
@@ -5,10 +5,77 @@
  */
 package com.humber.soapwebservice.services;
 
+import com.humber.soapwebservice.controllers.TvShowsJpaController;
+import com.humber.soapwebservice.models.TvShows;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.xml.ws.soap.MTOM;
+
 /**
  *
  * @author Dharmesh
  */
+@WebService
+@MTOM(enabled = true, threshold = 3000)
 public class TvShowsService {
     
+    @WebMethod
+    public TvShows addTvShow(@WebParam(name = "tvshowname") String tvshowName, @WebParam(name = "image") byte[] image) throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
+
+        TvShowsJpaController tvshowRepo = new TvShowsJpaController(emf);
+
+        TvShows tvshow = new TvShows();
+        tvshow.setName(tvshowName);
+        tvshow.setImage(image);
+        tvshow.setId(BigDecimal.valueOf(tvshowRepo.getTvShowsCount() + 1));
+
+        tvshowRepo.create(tvshow);
+
+        return tvshow;
+    }
+
+    @WebMethod
+    public List<TvShows> getTvShows() throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
+
+        TvShowsJpaController tvshowRepo = new TvShowsJpaController(emf);
+
+        List<TvShows> findTvShowsEntities = tvshowRepo.findTvShowsEntities(10, 0);
+
+        return findTvShowsEntities;
+    }
+
+    @WebMethod
+    public TvShows updateTvShow(@WebParam(name = "id") int id, @WebParam(name = "tvshowname") String tvshowName, @WebParam(name = "image") byte[] image) throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
+
+        TvShowsJpaController tvshowRepo = new TvShowsJpaController(emf);
+
+        TvShows findTvShows = tvshowRepo.findTvShows(BigDecimal.valueOf(id));
+        tvshowRepo.destroy(BigDecimal.valueOf(id));
+
+        findTvShows.setName(tvshowName);
+        findTvShows.setImage(image);
+        findTvShows.setId(BigDecimal.valueOf(tvshowRepo.getTvShowsCount() + 1));
+
+        tvshowRepo.create(findTvShows);
+
+        return findTvShows;
+    }
+
+    @WebMethod
+    public int deleteTvShowById(@WebParam(name = "id") int id) throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SOAP_PU");
+
+        TvShowsJpaController tvshowRepo = new TvShowsJpaController(emf);
+        tvshowRepo.destroy(BigDecimal.valueOf(id));
+
+        return id;
+    }
 }
